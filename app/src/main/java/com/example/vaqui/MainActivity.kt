d4d849d5456100264cdcc7308b6e3a7e6f30ab15
Alejft18 <joseon85@gmail.com>
2023-04-10 20:40:32 -0500

    arreglo navegation

--- app/src/main/java/com/example/vaqui/MainActivity.kt
+++ app/src/main/java/com/example/vaqui/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.vaqui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_home_inicio_sesion)
+        setContentView(R.layout.activity_main)
 
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment? ?: return
+        val navController = host.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupBottonNavMenu(navController)
+    }
+
+
+    private fun setupBottonNavMenu(navController: NavController){
+        val bottoNav = findViewById<BottomNavigationView>(R.id.btnMenu)
+        bottoNav?.setupWithNavController(navController)
     }
 }
