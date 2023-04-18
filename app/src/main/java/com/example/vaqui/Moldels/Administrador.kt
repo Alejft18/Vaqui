0429bd56222f154b7ff59d401c9cfd06b16a239e
stiveng2605 <103143726+stiveng2605@users.noreply.github.com>
2023-04-17 20:57:22 -0500

    Adaptadores

---
+++ app/src/main/java/com/example/vaqui/Moldels/Administrador.kt
@@ -0,0 +1,4 @@
+package com.example.vaqui.Moldels
+
+class Administrador {
+}
--- app/src/main/java/com/example/vaqui/adapter/bovinosAdapter.kt
+++ app/src/main/java/com/example/vaqui/adapter/bovinosAdapter.kt
@@ -1,36 +1,47 @@
-/*
+
 package com.example.vaqui.adapter
 
+import android.icu.text.Transliterator.Position
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import org.json.JSONObject
 import com.example.vaqui.adapter.BovinosListener
 import com.example.vaqui.*
 
-*/
-/*class bovinosAdapter (private val bovinosList: ArrayList<JSONObject>, private val bovinosListener: BovinosListener) : RecyclerView.Adapter<bovinosAdapter.ViewHolder>() {
+
+class bovinosAdapter (private val bovinosList: ArrayList<JSONObject>, private val bovinosListener: BovinosListener) : RecyclerView.Adapter<bovinosAdapter.ViewHolder>() {
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         var BovinosID : TextView=view.findViewById(R.id.BovinosID)
         var Raza : TextView=view.findViewById(R.id.Raza)
-    }*//*
+
+        fun bind(bovino: JSONObject){
+            BovinosID.text = bovino.getString("ID")
+            Raza.text = bovino.getString("Raza")
+        }
+    }
 
 
-*/
-/*    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) {
         LayoutInflater
             .from(parent.context)
             .inflate(R.layout.item_buscador, parent, false)
-    }*//*
+    }
 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val bovino = bovinosList[position]
+        try {
+            holder.itemView.setOnClickListener{
+                bovinosListener.onItemClicked(bovino, position)
+            }
+        } catch (e: Exception){
+            log.w("BovinoImagen", "No cargo la imagen")
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-}*/
+
+}
