7457ebb2df3bbd9ad598e9f1b9d53be305fc65ec
Alejft18 <joseon85@gmail.com>
2023-05-11 22:59:32 -0500

    implementacion de todos los detalles

--- app/src/main/java/com/example/vaqui/datos_sementales.kt
+++ app/src/main/java/com/example/vaqui/datos_sementales.kt
@@ -1,59 +1,79 @@
 package com.example.vaqui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
+import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONObject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class datos_sementales : DialogFragment() {
+    private lateinit var tvBarra_toro : Toolbar
+    private lateinit var recycler : RecyclerView
+    private lateinit var viewAlpha : View
+    private lateinit var rltoro : RelativeLayout
 
-/**
- * A simple [Fragment] subclass.
- * Use the [datos_sementales.newInstance] factory method to
- * create an instance of this fragment.
- */
-class datos_sementales : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var id_toro : TextView
+    private lateinit var peso_toro : TextView
+    private lateinit var fecha_extraccion_toro : TextView
+    private lateinit var vacas_montadas_toro : TextView
+    private lateinit var fecha_revision_toro : TextView
+    private lateinit var catego_toro : TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setStyle(STYLE_NORMAL,R.style.FullScreenDialogStyle)
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_datos_sementales, container, false)
+        val ll= inflater.inflate(R.layout.fragment_datos_sementales, container, false)
+        this.tvBarra_toro = ll.findViewById(R.id.tvBarra_toro)
+
+        this.id_toro = ll.findViewById(R.id.id_toro)
+        this.peso_toro = ll.findViewById(R.id.peso_toro)
+        this.fecha_extraccion_toro = ll.findViewById(R.id.fecha_extraccion_toro)
+        this.vacas_montadas_toro = ll.findViewById(R.id.vacas_montadas_toro)
+        this.fecha_revision_toro = ll.findViewById(R.id.fecha_revision_toro)
+        this.catego_toro = ll.findViewById(R.id.catego_toro)
+
+        this.recycler = ll.findViewById(R.id.toros_recycler)
+        this.viewAlpha = ll.findViewById(R.id.view_Toros)
+        this.rltoro = ll.findViewById(R.id.rl_Toros)
+
+        return ll
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this.tvBarra_toro.navigationIcon= ContextCompat.getDrawable(view.context,R.drawable.ic_baseline_close_24)
+        this.tvBarra_toro.setNavigationOnClickListener {
+            dismiss()
+        }
+
+    val toros = JSONObject(arguments?.getString("toros"))
+        this.id_toro.text=toros.getString("id")
+        this.peso_toro.text=toros.getString("peso_kilos")
+        this.fecha_extraccion_toro.text=toros.getString("fecha_extraccion")
+        this.vacas_montadas_toro.text=toros.getString("vacas_montadas")
+        this.fecha_revision_toro.text=toros.getString("fecha_revision")
+        this.catego_toro.text=toros.getString("categoria")
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment datos_sementales.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            datos_sementales().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 }
