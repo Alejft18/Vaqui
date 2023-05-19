00bace7daa5f78dd8f086824381f30add8d4bb44
stiveng2605 <esga1526@gmail.com>
2023-05-19 16:55:48 -0500

    gestion empleados

--- app/src/main/java/com/example/vaqui/GestionFragment.kt
+++ app/src/main/java/com/example/vaqui/GestionFragment.kt
@@ -1,59 +1,43 @@
 package com.example.vaqui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.navigation.Navigation
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GestionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GestionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var card_buscar_bovi : CardView
+    private lateinit var card_ingresar_bovino: CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gestion, container, false)
+        val ll = inflater.inflate(R.layout.fragment_gestion, container, false)
+        this.card_buscar_bovi = ll.findViewById(R.id.card_buscar_bovi)
+        this.card_ingresar_bovino = ll.findViewById(R.id.card_ingresar_bovino)
+
+        card_buscar_bovi.setOnClickListener{
+            val navController= Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_container)
+            navController.navigate(R.id.generalfragment2)
+        }
+
+        card_ingresar_bovino.setOnClickListener{
+            val navController= Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_container)
+            navController.navigate(R.id.formulario_general)
+        }
+
+        return ll
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GestionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GestionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
