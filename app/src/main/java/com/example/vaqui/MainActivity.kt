765d321d87025bb38eaf72976c4315ab004cb1e3
david <davidestebanmesa11@gmail.com>
2023-05-19 15:54:28 -0500

    Form General and MainActivity

--- app/src/main/java/com/example/vaqui/MainActivity.kt
+++ app/src/main/java/com/example/vaqui/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.vaqui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.ImageButton
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
@@ -14,10 +19,51 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
+    val genero = arrayOf("Macho","Hembra")
+    val procedencia = arrayOf("De este finca","De otra finca")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.genero)
+        val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,genero)
+        spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(applicationContext,"select genero is = " + genero[position], Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+
+        val spinnerP = findViewById<Spinner>(R.id.procedencia)
+        val arrayAdapterP = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,procedencia)
+        spinnerP.adapter = arrayAdapterP
+        spinnerP.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(applicationContext, "select procedencia is = " + procedencia[position], Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
 
         val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment? ?: return
         val navController = host.navController
@@ -31,7 +77,6 @@ class MainActivity : AppCompatActivity() {
             navController.navigate(R.id.perfilFragment)
         }
     }
-
 
     private fun setupBottonNavMenu(navController: NavController){
         val bottoNav = findViewById<BottomNavigationView>(R.id.btnMenu)
