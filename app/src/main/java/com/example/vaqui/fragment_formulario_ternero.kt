171ba72b0bb95dbd695812aab5c1c4d410dd51a8
david <davidestebanmesa11@gmail.com>
2023-05-30 16:12:25 -0500

    formulario ternero parte logica terminada

--- app/src/main/java/com/example/vaqui/fragment_formulario_ternero.kt
+++ app/src/main/java/com/example/vaqui/fragment_formulario_ternero.kt
@@ -1,32 +1,39 @@
 package com.example.vaqui
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.material.textfield.TextInputEditText
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.HashMap
+import java.util.Locale
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [fragment_formulario_ternero.newInstance] factory method to
- * create an instance of this fragment.
- */
 class fragment_formulario_ternero : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var ingreso_id_madre_ternero: TextInputEditText
+    private lateinit var ingreso_peso_ternero: TextInputEditText
+    private lateinit var ingreso_fechaRevision_ternero: TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val botonEnviar: Button = view.findViewById(R.id.boton_enviar_ternero)
+        botonEnviar.setOnClickListener { clickAddTernero(view) }
     }
 
     override fun onCreateView(
@@ -34,26 +41,75 @@ class fragment_formulario_ternero : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_formulario_ternero, container, false)
+        val view = inflater.inflate(R.layout.fragment_formulario_ternero, container, false)
+
+        ingreso_id_madre_ternero = view.findViewById(R.id.ingreso_id_madre_ternero)
+        ingreso_peso_ternero = view.findViewById(R.id.ingreso_peso_ternero)
+        ingreso_fechaRevision_ternero = view.findViewById(R.id.ingreso_fechaRevision_ternero)
+
+        //logica de las fechas (datePicker)
+        val myCalendar= Calendar.getInstance()
+
+        val datePicker= DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR,year)
+            myCalendar.set(Calendar.MONTH,month)
+            myCalendar.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            updateLable(myCalendar)
+        }
+
+        // Deshabilito el teclado en las fechas
+        ingreso_fechaRevision_ternero.apply {
+            isFocusable = false
+            isClickable = true   // Hacer el EditText clickeable
+        }
+
+        ingreso_fechaRevision_ternero.setOnClickListener {
+            val dialog = DatePickerDialog(requireContext(),datePicker,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH))
+            //pongo la fecha maxima como dia actual
+            dialog.datePicker.maxDate=Calendar.getInstance().timeInMillis
+
+            //pongo la fecha minima hasta el año 2000
+            val minDate = Calendar.getInstance()
+            minDate.set(2000, Calendar.JANUARY, 1)
+            dialog.datePicker.minDate = minDate.timeInMillis
+
+            dialog.show()
+        }
+
+        return view
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_formulario_ternero.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_formulario_ternero().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun updateLable(myCalendar: Calendar) {
+        val myformat = "yyyy-MM-dd"
+        val sdf = SimpleDateFormat(myformat, Locale("es","CO"))
+        ingreso_fechaRevision_ternero.setText(sdf.format(myCalendar.time))
+    }
+
+    private fun clickAddTernero(view: View) {
+        val url="http://192.168.95.187/phpVaqui/agregar_ternero.php"
+        val queue = Volley.newRequestQueue(requireContext())
+        val resultadoPost = object : StringRequest(Request.Method.POST, url,
+            Response.Listener<String> { response->
+                Toast.makeText(requireContext(), "Ternero ingresada exitosamente", Toast.LENGTH_LONG).show()
+            }, Response.ErrorListener{
+                Toast.makeText(requireContext(), "Ternero no agregado", Toast.LENGTH_LONG).show()
             }
+        ){
+            override fun getParams(): MutableMap<String, String>? {
+                val parametros = HashMap<String, String>()
+
+                parametros.put("id_madre",ingreso_id_madre_ternero?.text.toString())
+                parametros.put("peso_kilos",ingreso_peso_ternero?.text.toString())
+                parametros.put("fecha_revision ",ingreso_fechaRevision_ternero?.text.toString())
+
+                Log.d("error", "$parametros")
+                Log.d("error", "error")
+                return parametros
+
+            }
+        }
+        queue.add(resultadoPost)
+
     }
 }
