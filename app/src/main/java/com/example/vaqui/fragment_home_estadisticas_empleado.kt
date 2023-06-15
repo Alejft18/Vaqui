7a6513f7cf70ffcbadb45320c69e46fb1c5e6f60
Alejft18 <joseon85@gmail.com>
2023-06-15 10:21:00 -0500

    cambio de imagenes de slider

--- app/src/main/java/com/example/vaqui/fragment_home_estadisticas_empleado.kt
+++ app/src/main/java/com/example/vaqui/fragment_home_estadisticas_empleado.kt
@@ -29,10 +29,11 @@ class fragment_home_estadisticas_empleado : Fragment() {
         val imageSlider = ll.findViewById<ImageSlider>(R.id.image_slider)
 
 
-        imageList.add(SlideModel("https://definicion.de/wp-content/uploads/2016/05/bovino-1.jpg","vaca 1"))
-        imageList.add(SlideModel("https://cdn.shopify.com/s/files/1/0268/6861/files/bulls-219952_960_720_00de38ab-1909-453b-bbc0-090de89dbba4_grande.jpg?v=1559242513","vaca 2"))
-        imageList.add(SlideModel("https://t1.ea.ltmcdn.com/es/posts/4/6/2/brucelosis_bovina_sintomas_y_tratamiento_24264_orig.jpg", "vaca 3"))
-        imageList.add(SlideModel("https://bmeditores.mx/wp-content/uploads/2021/07/Bienestar-animal-bovinos-lecheros-3.jpg","vaca 4"))
+        imageList.add(SlideModel(R.drawable.imagen_vacas_1,"vaca 1"))
+        imageList.add(SlideModel(R.drawable.imagen_vacas_2,"vaca 2"))
+        imageList.add(SlideModel(R.drawable.imagen_vacas_3,"vaca 3"))
+        imageList.add(SlideModel(R.drawable.imagen_vacas_4,"vaca 4"))
+        imageList.add(SlideModel(R.drawable.imagen_vacas_5,"vaca 5"))
 
         imageSlider.setImageList(imageList)
 
