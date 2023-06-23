6bf8ce2b18cee240d73e450d977ec5e22a1727d5
Alejft18 <joseon85@gmail.com>
2023-06-23 00:42:07 -0500

    implementacion estadisticas del inicio

--- app/src/main/java/com/example/vaqui/fragment_home_estadisticas_empleado.kt
+++ app/src/main/java/com/example/vaqui/fragment_home_estadisticas_empleado.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.navigation.fragment.findNavController
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.models.SlideModel
 
 class fragment_home_estadisticas_empleado : Fragment() {
     private lateinit var card_gestion_empleados : CardView
-    val imageList=ArrayList<SlideModel>()
+    val imageList = ArrayList<SlideModel>()
+    private lateinit var promedio_leche : TextView
+    private lateinit var cantidad_bovinos : TextView
+    private lateinit var cantidad_terneros : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +33,9 @@ class fragment_home_estadisticas_empleado : Fragment() {
         // Inflate the layout for this fragment
         val ll = inflater.inflate(R.layout.fragment_home_estadisticas_empleado, container, false)
         this.card_gestion_empleados = ll.findViewById(R.id.card_gestion_empleados)
+        this.promedio_leche = ll.findViewById(R.id.promedio_leche)
+        this.cantidad_bovinos = ll.findViewById(R.id.cantidad_bovinos)
+        this.cantidad_terneros = ll.findViewById(R.id.cantidad_terneros)
 
         val imageSlider = ll.findViewById<ImageSlider>(R.id.image_slider)
 
@@ -37,7 +48,9 @@ class fragment_home_estadisticas_empleado : Fragment() {
 
         imageSlider.setImageList(imageList)
 
-
+        promedioLeche()
+        cantidadBovinos()
+        cantidadTerneros()
 
         return ll
     }
@@ -48,10 +61,65 @@ class fragment_home_estadisticas_empleado : Fragment() {
         card_gestion_empleados.setOnClickListener{
             findNavController().navigate(R.id.action_inicio_to_gestionEmpleadosFragment)
         }
-
-
-
     }
 
+    private fun cantidadTerneros(){
+        val url = "http://192.168.123.187:8080/cantidadTerneros"
+        val queue = Volley.newRequestQueue(requireContext())
+
+        val request = StringRequest(
+            Request.Method.GET, url,
+            { response ->
+                val cantidadTerneros = response.toIntOrNull()
+                if (cantidadTerneros != null){
+                    cantidad_terneros.text = cantidadTerneros.toString()
+                }else{
+                    Toast.makeText(requireContext(), "Respuesta inválida del servidor", Toast.LENGTH_LONG).show()
+                }
+            }
+        ) { error ->
+            Toast.makeText(requireContext(), "Error al obtener cantidad terneros", Toast.LENGTH_LONG).show()
+            println("Error en la solicitud: " + error.message)
+        }
+        queue.add(request)
+    }
+
+
+    private fun cantidadBovinos(){
+        val url = "http://192.168.123.187:8080/cantidadBovinos"
+        val queue = Volley.newRequestQueue(requireContext())
+
+        val request = StringRequest(Request.Method.GET, url,
+            { response ->
+                val cantidadBovinos = response.toIntOrNull()
+                if (cantidadBovinos != null){
+                    cantidad_bovinos.text = cantidadBovinos.toString()
+                }else{
+                    Toast.makeText(requireContext(), "Respuesta inválida del servidor", Toast.LENGTH_LONG).show()
+                }
+            }
+        ) { error ->
+            Toast.makeText(requireContext(), "Error al obtener cantidad terneros", Toast.LENGTH_LONG).show()
+            println("Error en la solicitud: " + error.message)
+        }
+        queue.add(request)
+    }
+
+
+    private fun promedioLeche(){
+        val url = "http://192.168.123.187:8080/promedioLeche"
+        val queue = Volley.newRequestQueue(requireContext())
+
+        val request = StringRequest(Request.Method.GET, url,
+            { response ->
+                val promedioLeche = response.toString()
+                promedio_leche.text = promedioLeche
+            }
+        ) { error ->
+            Toast.makeText(requireContext(), "Error al obtener cantidad terneros", Toast.LENGTH_LONG).show()
+            println("Error en la solicitud: " + error.message)
+        }
+        queue.add(request)
+    }
 
 }
